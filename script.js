f2dab74fb9786bd7b69901facf015e60f22b5632
Jason Chan <jlc370@gmail.com>
2015-12-17 15:16:18 -0500

    save before raid boss

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ var field = $('#playField');
 var x = field.innerWidth();
 var y = 900;
 var fieldDimensions = [x,y];
-console.log(fieldDimensions);
-//this var to control game playing
+
+//these var to control game playing
 //can use with clear interval.
 var incoming;
-
 var path;
 
 //difficulty control
@@ -20,65 +19,108 @@ var interval = 5000;
 //score tracker
 var score = 0;
 
-  //create a new missile
-  //location subtract a little over img div size
-  function makeMissile () {
-    var missile = $('<div class="missile"></div>');
-    field.append(missile);
+//anti-missile location control
+var left;
+var top;
+
+//create a new missile
+//location subtract a little over img div size
+function makeMissile () {
+  var missile = $('<div class="missile"></div>');
+     field.append(missile);
   var missileSpawn = Math.ceil(Math.random() * (x-55));
   var missileTarget = Math.ceil(Math.random() * (x-55));
   var spin=90;
-  //generate it to a random location on TOP of the field & rotate!
+
+  //rotate!
   if ((missileTarget - missileSpawn)>0) {
     spin = spin +(180*Math.atan(700/(missileTarget - missileSpawn)))/Math.PI;
   } else {
-    spin = spin + 90 +(180*Math.atan(700/(missileSpawn - missileTarget)))/Math.PI;
+    spin = spin + 90 +(180*Math.atan((missileSpawn - missileTarget)/700))/Math.PI;
   }
+
+  //spawn missile at location, rotated
   missile.css({"top":0,
                "left":missileSpawn,
                "transition":"top "+timer+"s, left "+timer+"s",
                "transition-timing-function":"linear",
                "transform":"rotate("+spin+"deg)"});
-  //move the missile to a random target location
-  //HAVE TO ADJUST LOCATION WITH IMG DIV SIZE!!!
 
-  //sets target
+  //sets random target
   path = setTimeout(function(){
     var s= missile.css({"top":y-200,
                "left":missileTarget});
-  
   }, 10)
 
-  //Generate more, faster missiles over time
+  //generate more, faster missiles over time
   if(interval>500){
-    interval-=100;
+    interval-=250;
   }
   if(interval%1000===0&&timer>2){
     timer --;
   }
-
+  //end of generator
   }
+//create and shoot interceptor missile
+function interceptorMissile () {
+  var missile2 = $('<div class="missile2"></div>');
+    missile2.css({"top":800,
+               "left": x/2,
+               "transition":"top "+0.5+"s, left "+0.5+"s",
+               "transition-timing-function":"linear",
+             })
+  field.append(missile2);
+    window.setTimeout(function(){
+      var q= missile2.css({"top":top,
+               "left":left});
+    },10)
+}
+
+//remove interceptor missile when its job is done
+$(document).on('transitionend','.missile2',function(event){
+   $(this).remove();
+})
+//activate the missile generator at times
 var incoming = window.setInterval(makeMissile,interval);
 
-
 //missile click up score
+//also trigger stop missile and interceptor missile
+var hit;
 $(document).on('click','.missile',function(event){
-    score+=5;
-    $(".score").text(score);
-    $(this).addClass("boom")
-    return false;
+  $(".score").text(score);
+  hit = $(this);
+  var loc = window.getComputedStyle(this, null);
+  left = loc.getPropertyValue('left');
+  top = loc.getPropertyValue('top')
+  $(this).css({
+                "top": top,
+               "left": left,
+              });
+
+  hitMissile();
+  interceptorMissile();
+  // return false;
   })
 
+//interceptor missile add tag to target missile for destruction
+function hitMissile(){
+  window.setTimeout(function(){
+    hit.addClass("boom");
+  },500);
+ }
+  
 //explode missile
 function kaBoom(){
-    window.setTimeout(function(){
-      $(".boom").remove();
-    },1000);
+  score += 5;
+  window.setTimeout(function(){
+    $(".boom").remove();
+  },1000);
 }
 
 //to run the explode missile function
 $(document).on('click','.missile',kaBoom);
 
+//Game over man, Game over!
 $(document).on('transitionend','.missile',function(event){
    window.clearInterval(incoming);
   field.html("<br>GAME OVER!!!<br><br> Your Score is:<br>"+score);
