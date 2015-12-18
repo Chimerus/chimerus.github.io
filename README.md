be5828911e660eb4b6b37267a2e593626308afd0
Jason Chan <jlc370@gmail.com>
2015-12-18 18:00:33 -0500

    save

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #Missile Command
+##By Jason Chan 
 
 This is a version of the classic Missile Command using HTML, Javascript, and CSS.
 
@@ -6,7 +7,13 @@ As the game progresses, missiles will spawn and move faster! Click them with you
 
 
 Asset Acknowledgment:
+
+Images:
 http://vignette2.wikia.nocookie.net/metalslug/images/a/a5/Upgrade_missle.gif/revision/latest?cb=20101222041405
+http://vignette3.wikia.nocookie.net/metalslug/images/d/d4/Cohete-Chico.gif/revision/latest?cb=20101218194137
+http://www.motherjones.com/files/nuclear-explosion.gif
+Sounds:
 http://soundbible.com/709-Bottle-Rocket.html
 http://soundbible.com/696-Grenade.html
 http://soundbible.com/1847-Bunker-Buster-Missile.html
+Starship Troopers OST - Klendathu Drop
--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var fieldDimensions = [x,y];
 var launch = new Audio('/Sounds/Bottle Rocket-SoundBible.com-332895117.mp3');
 var impact = new Audio('/Sounds/Grenade-SoundBible.com-1777900486.mp3');
 var gameOver = new Audio('/Sounds/Bunker_Buster_Missle-Mike_Koenig-1405344373.mp3')
+var theme = new Audio('/Sounds/ThemeEdit.mp3');
+theme.play();
 
 //these var to control game playing
 //can use with clear interval.
@@ -22,13 +24,86 @@ var path;
 var timer = 7 ;
 var interval = 3500;
 
+//Modes
+$(".classic").on('click', function() {
+  gameType="Classic";
+  gameOn();
+})
+
+$(".challenge").on('click', function() {
+  timer = 4;
+  interval = 2000;
+  multiplier = 10;
+  gameType="Challenge";
+  gameOn();
+})
+
+$(".double").on('click', function() {
+  multiplier = 10;
+  gameType="Double Trouble";
+  gameDouble();
+})
+
+//game history:
+var hiScore="0";
+
+switch(gameType){
+  case "Classic":
+    if(!localStorage.getItem('classicScores')) {
+      populateStorage();
+    } else {
+      setScores('classicScores');
+    }
+    break;
+  case "Challenge":
+    if(!localStorage.getItem('challengeScores')) {
+      populateStorage();
+    } else {
+      setScores('challengeScores');
+    }
+    break;
+  case "Double Trouble":
+    if(!localStorage.getItem('doubleTroubleScores')) {
+      populateStorage();
+    } else {
+      setScores('doubleTroubleScores');
+    }
+    break;
+}
+
+function populateStorage(){
+  hiScore="0";
+}
+function setScores(x){
+  hiScore=localStorage.getItem(x);
+}
 //score tracker
 var score = 0;
+var multiplier = 5;
+var gameType;
+// var hiScore;
 
 //anti-missile location control
 var left;
 var top;
 
+$("#gameOver").hide();
+
+//Game Start
+function gameOn() {
+  $("#screenCover").hide();
+  incoming = window.setInterval(makeMissile,interval);
+}
+
+function gameDouble() {
+  $("#screenCover").hide();
+  incoming = window.setInterval(makeTwoMissile,interval);
+}
+
+function makeTwoMissile() {
+    makeMissile();
+    makeMissile();
+}
 //create a new missile
 //location subtract a little over img div size
 function makeMissile () {
@@ -62,21 +137,31 @@ function makeMissile () {
   if(interval>500){
     interval-=250;
   }
-  if(interval%1000===0&&timer>2){
+  if(interval%1000===0&&timer>1){
     timer --;
   }
   //end of generator
   missile.one("click", missileClickHandler );
-
   }
+
 //create and shoot interceptor missile
 function interceptorMissile () {
+  //not rotating... 
+  // var spin2=180;
+  // //rotate!
+  // if ((left - x/2)<0) {
+  //   spin2 = spin2 +(180*Math.atan((700-top)/(left-x/2)))/Math.PI;
+  // } else {
+  //   spin2 = spin2 + 90 +(180*Math.atan((x/2-left)/700))/Math.PI;
+  // }
+
   var missile2 = $('<div class="missile2"></div>');
     launch.play();
     missile2.css({"top":800,
                "left": x/2,
                "transition":"top "+0.5+"s, left "+0.5+"s",
                "transition-timing-function":"linear",
+               //"transform":"rotate("+spin2+"deg)",
              })
   field.append(missile2);
     window.setTimeout(function(){
@@ -89,14 +174,12 @@ function interceptorMissile () {
 $(document).on('transitionend','.missile2',function(event){
    $(this).remove();
 })
-//activate the missile generator at times
-var incoming = window.setInterval(makeMissile,interval);
 
 //missile click up score
 //also trigger stop missile and interceptor missile
 var hit;
 var missileClickHandler = function(event){
-  score += 5;
+  score += multiplier;
   $(".score").text("Score: "+score);
   hit = $(this);
   var loc = window.getComputedStyle(this, null);
@@ -106,10 +189,8 @@ var missileClickHandler = function(event){
                 "top": top,
                "left": left,
               });
-
   hitMissile();
   interceptorMissile();
-  // return false;
 }
 
 //interceptor missile add tag to target missile for destruction
@@ -130,11 +211,52 @@ function kaBoom(){
 //to run the explode missile function
 $(document).on('click','.missile',kaBoom);
 
+function scoring(){
+  var temp=score.toString();
+  hiScore=hiScore+" "+temp;
+  hiScore=hiScore.split(" ").sort(function(a, b) {
+    return b - a;
+  });
+  if(hiScore.length>9){
+    hiScore = hiScore.pop();
+  }
+  hiScore=hiScore.join(" ");
+}
 //Game over man, Game over!
 $(document).on('transitionend','.missile',function(event){
-   window.clearInterval(incoming);
-   gameOver.play();
-  field.html("<br>GAME OVER!!!<br><br> Your Score is:<br>"+score);
+  window.clearInterval(incoming);
+  $(".missile").remove();
+  scoring();
+  window.setTimeout(function(){
+    theme.pause();
+  },1000);
+  gameOver.play();
+  $("#gameOver").show();
+
+//store new hi score list  
+switch(gameType){
+  case "Classic":
+    localStorage.setItem('classicScores',hiScore);
+    break;
+  case "Challenge":
+    localStorage.setItem('challengeScores',hiScore);
+    break;
+  case "Double Trouble":
+    localStorage.setItem('doubleTroubleScores',hiScore);
+    break;
+}
+
+//score display
+//add Hi score list
+var disp = hiScore.split(" ");
+var hiScoreDisplay="";
+for(var i=0;i<disp.length;i++){
+  hiScoreDisplay+=disp[i]+"<br>";
+}
+
+//display your score and Hi score list
+var stock = "<br>GAME OVER!!!<br><br> Your Score is:<br>"+score+"<br><br> HIGH SCORE LIST:<br>";
+$("#gameOver").html(stock+hiScoreDisplay);
 })
 
 })();
